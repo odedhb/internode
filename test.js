80caca1c70ae8941f4b335e070fbcd7275804bf1
odedhb <odedhb@gmail.com>
2017-08-07 16:14:21 +0300

    Testing: for app.js

--- test.js
+++ test.js
@@ -2,10 +2,28 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const internode_1 = require("./internode");
 class TestApp {
-    get() {
-        console.log('getCalled');
+    get(syncPath, callback) {
+        console.log('app init ' + syncPath);
+        this.syncCallback = callback;
+        let req = { query: { node_id: 3 } };
+        this.syncCallback(req, new TestResponse());
     }
 }
+class TestResponse {
+    send(message) {
+        console.log('Response: ' + JSON.stringify(message));
+    }
+}
+////
 internode_1.InterNode.init(new TestApp(), null);
+////
 internode_1.InterNode.set('test1', '1');
+if (internode_1.InterNode.itemCount() == 1) {
+    console.log('set() success');
+}
+////
+if (internode_1.InterNode.get('test1') == `1`) {
+    console.log('get() success');
+}
+////
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -2,10 +2,31 @@ import {InterNode} from "./internode";
 
 
 class TestApp {
-    get() {
-        console.log('getCalled');
+    syncCallback: any;
+
+    get(syncPath: string, callback: any) {
+        console.log('app init ' + syncPath);
+        this.syncCallback = callback;
+        let req = {query: {node_id: 3}};
+        this.syncCallback(req, new TestResponse());
     }
 }
 
+class TestResponse {
+    send(message: any) {
+        console.log('Response: ' + JSON.stringify(message));
+    }
+}
+
+////
 InterNode.init(new TestApp(), null);
+////
 InterNode.set('test1', '1');
+if (InterNode.itemCount() == 1) {
+    console.log('set() success');
+}
+////
+if (InterNode.get('test1') == `1`) {
+    console.log('get() success');
+}
+////
